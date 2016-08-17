3bec65de3d46f59a0d755302e7cac4b1c55d4e36
Daniel Knuettel <daknuett@gmail.com>
2016-08-17 18:46:07 +0200

    added better doc

--- engine_tools/operations.py
+++ engine_tools/operations.py
@@ -7,13 +7,15 @@ Operations used by the engine.
 
 def bitsetxor(b1, b2):
 	"""
-	If b1 and b2 would be ``int``s this would be ``b1 ^ b2``:
+	If b1 and b2 would be ``int`` s this would be ``b1 ^ b2`` :
 
 	>>> from py_register_machine2.engine_tools.operations import bitsetxor
 	>>> b1 = [1, 1, 1, 1]
 	>>> b2 = [1, 1, 0, 1]
 	>>> bitsetxor(b1, b2)
 	[0, 0, 1, 0]
+	>>> bin(0b1111 ^ 0b1101)
+	'0b10'
 	"""
 	res = []
 	for bit1, bit2 in zip(b1, b2):
